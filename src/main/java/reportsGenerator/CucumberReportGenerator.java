5447188c561dd4c55cfa51f446d5c491bb9a9412
Ralph Matienzo <122351486+rlmmatienzo@users.noreply.github.com>
2025-12-01 18:37:26 +0800

    Initial commit of code

---
+++ src/main/java/reportsGenerator/CucumberReportGenerator.java
@@ -0,0 +1,19 @@
+package reportsGenerator;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+public class CucumberReportGenerator {
+	public static void generateReport(String jsonFilePath, String reportOutputPath, String projectName) {
+		File reportOutputDirectory = new File(reportOutputPath);
+		List<String> jsonFiles = Collections.singletonList(jsonFilePath);
+
+		Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+		ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+		reportBuilder.generateReports();
+	}
+}
